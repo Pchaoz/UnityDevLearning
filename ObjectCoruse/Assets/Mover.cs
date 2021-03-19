44ece0ecd0b17a89afa3adf8b9405044cb1f9acd
Pchaoz <51079893+Pchaoz@users.noreply.github.com>
2021-03-19 20:16:31 +0100

    Subida la carpeta del proyecto "ObjectCourse"

---
+++ ObjectCoruse/Assets/Mover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Variable que guarda el input para moverse a derecha o izquierda todo el rato (x)
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        
+        //Variable que guarda el input para moverse a adelant o atras todo el rato (z)
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+
+        transform.Translate(xValue, 0, zValue);
+
+    }
+}
