a20c210701c616d298a3ec6892d55d5207513039
Pchaoz <51079893+Pchaoz@users.noreply.github.com>
2021-03-21 00:20:17 +0100

    Updated game 1.0
    - Ahora el personaje cuando choca contra objetos se suma en una variable y sale printado por consola
    
    - Las paredes cambian de color a gris la primera vez que chocas

--- ObjectCoruse/Assets/Mover.cs
+++ ObjectCoruse/Assets/Mover.cs
@@ -8,23 +8,39 @@ public class Mover : MonoBehaviour
     [SerializeField] float moveSpeed = 15;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         
+        PrintInstructions();
+
     }
 
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
+
+        MovePlayer();
+
+    }
+
+    void PrintInstructions() {
+
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player with W,A,S,D or arrow keys");
+        Debug.Log("Don't hit the walls");
+    
+    }
+
+    void MovePlayer() {
+
         //Variable que guarda el input para moverse a derecha o izquierda todo el rato (x)
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         
         //Variable que guarda el input para moverse a adelant o atras todo el rato (z)
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
-
+        //Variable que mueve el jugador
         transform.Translate(xValue, 0, zValue);
 
     }
+
 }
---
+++ ObjectCoruse/Assets/ObjectHit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHit : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other) {
+        
+        Debug.Log("Bumped into a wall");
+        GetComponent<MeshRenderer>().material.color = Color.gray;
+
+    }
+}
